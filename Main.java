be040014212deb4cdac6c969ce2f5279b9e8bfc7
savannahlut <savannahlut@nycstudents.net>
2024-10-09 13:32:53 +0000

    test

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        WordChecker w = new WordChecker(); 
+        ArrayList<String> words = new ArrayList<String>();
+    }
+}
