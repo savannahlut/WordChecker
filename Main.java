6623229a7d9f096956554051bc8a5afee49968f2
savannahlut <savannahlut@nycstudents.net>
2024-10-13 05:52:14 +0000

    start create list

--- Main.java
+++ Main.java
@@ -3,14 +3,29 @@ public class Main
 {
     public static void main(String[] args)
     {
-        WordChecker w = new WordChecker(); 
         ArrayList<String> words = new ArrayList<String>();
         words.add("an");
         words.add("band");
         words.add("band");
         words.add("abandon");
-        System.out.println(words);
         WordChecker x = new WordChecker(words);
-        
+        System.out.println(x.isWordChain());
+
+        ArrayList<String> more = new ArrayList<String>();
+        more.add("to");
+        more.add("too");
+        more.add("stool");
+        more.add("tools");
+        WordChecker y = new WordChecker(more);
+        System.out.println(y.isWordChain());
+
+        ArrayList<String> beauty = new ArrayList<String>();
+        beauty.add("catch");
+        beauty.add("bobcat");
+        beauty.add("catchacat");
+        beauty.add("cat");
+        beauty.add("at");
+        WordChecker z = new WordChecker(beauty);
+        System.out.println(z.createList("cat"));
     }
 }
--- WordChecker.java
+++ WordChecker.java
@@ -13,10 +13,21 @@ public class WordChecker {
     {
         for(int i = 1; i < wordList.size(); i++)
         {
-            boolean wordChain = true; 
             String after = wordList.get(i); 
             String before = wordList.get(i - 1); 
-            if (after.indexOf(before) < 0) return false;
+            if (after.indexOf(before) < 0) return false; 
         }
+        return true;
+    }
+    public ArrayList<String> createList(String target)
+    {
+        ArrayList<String> newList = new ArrayList<String>();
+        if (wordList.get(0) == target)
+        {
+            String after = wordList.get(0);
+            after = after.substring(3, after.length());
+            newList.add(after);
+        }
+        return newList; 
     }
 }
